a1e012fef33344063dc2931c9a0f57292d1447fa
Vladimir Shuabrin <vhundef@gmail.com>
2019-03-21 09:55:34 +0300

    Updated copyright notices

---
+++ Lab3/Lab3_1.cpp
@@ -0,0 +1,7 @@
+/* Copyright (C) Vladimir Shubarin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Vladimir Shubarin <vhundef@gmail.com>, Mar 2019
+ */
+
+#include "Lab3_1.h"
---
+++ Lab3/Lab3_1.h
@@ -0,0 +1,15 @@
+/* Copyright (C) Vladimir Shubarin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Vladimir Shubarin <vhundef@gmail.com>, Mar 2019
+ */
+#ifndef PROGONCPP_LAB3_1_H
+#define PROGONCPP_LAB3_1_H
+
+
+class Lab3_1 {
+
+};
+
+
+#endif //PROGONCPP_LAB3_1_H
