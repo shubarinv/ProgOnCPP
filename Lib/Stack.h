e38356e107566106b5ca4a9fec9e382fcd2189da
Vladimir Shubarin <vhundef@gmail.com>
2019-05-12 22:28:02 +0300

    lab 5_2 Updated,stack fixed

--- Lib/Stack.h
+++ Lib/Stack.h
@@ -1,66 +1,79 @@
-//
-// Created by vhund on 24.03.2019.
-//
 
-#ifndef PROGONCPP_STACK_H
-#define PROGONCPP_STACK_H
 
-#include <iomanip>
+
+#include <new>
 #include <iostream>
 
-using namespace std;
-typedef int DataType;
-
-template <class Tdata> class Stack {
+template<typename T>class Stack {
 private:
-  struct Element {
-    Tdata data;
-    Element *next;
-  } * pstack;
-
+	struct Element {
+		T data;
+		Element *next;
+	} *pstack;
 public:
-  Stack() { pstack = nullptr; }
+	Stack();
 
-  ~Stack();
+	~Stack();
 
-  int Empty();
+	int Empty();
 
-  Tdata Pop();
+	T Top();
 
-  int Push(Tdata x);
+	T Pop();
 
-  Tdata showData();
+	T Push(T x);
+
+	void stkPrint();
 };
 
-template <class Tdata> Stack<Tdata>::~Stack() {
-  Element *temp;
-  while (pstack) {
-    temp = pstack;
-    pstack = pstack->next;
-    delete temp;
-  }
+template<typename T>
+Stack<T>::Stack() {
+	pstack = nullptr;
 }
 
-template <class Tdata> int Stack<Tdata>::Empty() { return pstack == nullptr; }
-
-template <class Tdata> Tdata Stack<Tdata>::Pop() {
-  Element *del = pstack;
-  DataType temp = pstack->data;
-  pstack = pstack->next;
-  delete del;
-  return temp;
+template<typename T>
+Stack<T>::~Stack() {
+	Element *temp;
+	while (pstack) {
+		temp = pstack;
+		pstack = pstack->next;
+		delete temp;
+	}
 }
 
-template <class Tdata> int Stack<Tdata>::Push(Tdata x) {
-  auto ins = new (nothrow) Element;
-  if (ins == NULL)
-    return 0;
-  ins->data = x;
-  ins->next = pstack;
-  pstack = ins;;
-  return 1;
+template<typename T>
+int Stack<T>::Empty() {
+	return pstack == nullptr;
 }
 
-template <class Tdata> Tdata Stack<Tdata>::showData() { return pstack->data; }
+template<typename T>
+T Stack<T>::Top() {
+	return pstack->data;
+}
 
-#endif // PROGONCPP_STACK_H
+template<typename T>
+T Stack<T>::Pop() {
+	Element *del = pstack;
+	T temp = pstack->data;
+	pstack = pstack->next;
+	delete del;
+	return temp;
+}
+
+template<typename T>
+T Stack<T>::Push(T x) {
+	Element *ins = new(std::nothrow) Element;
+	if (ins == nullptr) return 0;
+	ins->data = x;
+	ins->next = pstack;
+	pstack = ins;
+	return 1;
+}
+template<typename T> void Stack<T>::stkPrint() {
+	Element* temp=pstack;
+	while(temp!= nullptr){
+		std::cout<<temp->data<<std::endl;
+		temp=temp->next;
+
+	}
+}
